8729d64054618212014fadc9f75500dd21e199bf
davew <dave@warnock.me.uk>
2016-02-29 22:45:12 +0000

    code cleanup

--- bee42.py
+++ bee42.py
@@ -23,38 +23,39 @@ class Beeminder:
         self.token = self.ini_beeminder[BEE42_INI_TOKEN]
         self.api_url = self.ini_beeminder[BEE42_INI_API_URL]
 
-    def user_url(self):
-        return self.api_url + "users/" + self.username + ".json"
+        self.user_url = self.api_url + "users/" + self.username + ".json"
 
     def goal_url(self, goal):
         return self.api_url + "users/" + self.username + "/goals/" + goal + ".json"
-
+        
     def datapoint_url(self, goal):
         return self.api_url + "users/" + self.username + "/goals/" + goal + "/datapoints.json"
 
     def authparam(self):
         return {'auth_token': self.token}
 
-    def yesterdayparam(self, comment, value):
-        return {
-            'auth_token': self.token,
-            'daystamp': datetime.date.today() - datetime.timedelta(days=1),
-            'value': value,
-            'comment': comment,
-            }
-
     def nowparam(self, comment, value):
-        return {
-            'auth_token': self.token,
+        n = {
             'value': value,
             'comment': comment,
             }
-        
+        n.update(self.authparam())
+        return n
+
+    def yesterdayparam(self, comment, value):
+        y = {
+            'daystamp': datetime.date.today() - datetime.timedelta(days=1),
+            }
+        y.update(self.nowparam(comment, value))
+        return y
+
     def loadUser(self):
-        r = requests.get(self.user_url(), params=self.authparam())
+        r = requests.get(self.user_url, params=self.authparam())
         return r.json()
 
 if __name__ == "__main__":
     b = Beeminder()
     print ("User:", b.user)
     print ("My Goals:", b.goals)
+    print ("Yesterday:")
+    print (b.yesterdayparam("cccc",111))
