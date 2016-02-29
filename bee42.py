09ee1f4384a75788fab75ef3a808c16389b0d2ea
davew <dave@warnock.me.uk>
2016-02-29 22:59:28 +0000

    added getGoalCurValue and getGoal

--- bee42.py
+++ bee42.py
@@ -8,6 +8,8 @@ BEE42_INI_USERNAME  = "Username"
 BEE42_INI_TOKEN     = "Token"
 BEE42_INI_API_URL   = "Api_Url"
 
+BEE42_WEIGHT_GOAL   = "Weight_Goal"
+
 class Beeminder:
     def __init__(self):
         self.load_config()
@@ -22,6 +24,8 @@ class Beeminder:
         self.username = self.ini_beeminder[BEE42_INI_USERNAME]
         self.token = self.ini_beeminder[BEE42_INI_TOKEN]
         self.api_url = self.ini_beeminder[BEE42_INI_API_URL]
+        
+        self.weight_goal = self.ini_beeminder[BEE42_WEIGHT_GOAL]
 
         self.user_url = self.api_url + "users/" + self.username + ".json"
 
@@ -53,9 +57,18 @@ class Beeminder:
         r = requests.get(self.user_url, params=self.authparam())
         return r.json()
 
+    def getGoal(self, goal):
+        r = requests.get(self.goal_url(goal), params=self.authparam())
+        return r.json()
+
+    def getGoalCurValue(self, goal):
+        g = self.getGoal(goal)
+        return g["curval"]
+      
 if __name__ == "__main__":
     b = Beeminder()
     print ("User:", b.user)
     print ("My Goals:", b.goals)
-    print ("Yesterday:")
-    print (b.yesterdayparam("cccc",111))
+    print ("Weight Goal:")
+    print (b.getGoal(b.weight_goal))
+    print ("Current Weight:", b.getGoalCurValue(b.weight_goal))
